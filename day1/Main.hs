91a80a63a4a913ed57b58e8498687a217b0d0b6f
Bill Cauchois <wcauchois@gmail.com>
2019-12-06 20:29:20 -0500

    part 2

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,9 +1,15 @@
 #!/usr/bin/env runghc
 
+getFuel :: Int -> Int
+getFuel x = x `div` 3 - 2
+
+totalFuel :: Int -> Int
+totalFuel = sum . tail . takeWhile (>0) . iterate getFuel
+
 main :: IO ()
 main = do
   inputString <- readFile "input.txt"
   let inputLines = lines inputString
   let inputInts = map read inputLines :: [Int]
-  let fuelValues = map (\x -> x `div` 3 - 2) inputInts
+  let fuelValues = map totalFuel inputInts
   print $ sum fuelValues
